3cca1b68e479a56ddd422385b409e25fafc52395
kare-tabetai <yahoo.zip.lzh.exe@gmail.com>
2024-11-10 16:23:51 +0900

    タイトル画面をを少し調整

--- Assets/Asae/Script/TitleAnimation.cs
+++ Assets/Asae/Script/TitleAnimation.cs
@@ -9,16 +9,30 @@ public class TitleAnimation : MonoBehaviour
     private Transform titleLogoObj = null;
     [SerializeField]
     private Transform titleButtonObj = null;
-    // Start is called before the first frame update
+    [SerializeField]
+    private TMPro.TextMeshProUGUI click_to_start_text;
+    [SerializeField]
+    private float text_flash_interval;
+
+    float text_flash_timer;
     void Start()
     {
         titleLogoObj.DOMoveY(753, 2f);
         titleButtonObj.DOMoveY(331, 2f);
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        text_flash_timer += Time.deltaTime;
+        if(text_flash_interval <= text_flash_timer)
+        {
+            click_to_start_text.enabled = !click_to_start_text.enabled;
+            text_flash_timer = 0;
+        }
+    }
+
+    public void StartButtonClicked()
+    {
+        MainGameManager.Instance.StartMainGame();
     }
 }
