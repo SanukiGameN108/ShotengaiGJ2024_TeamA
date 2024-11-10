e4d1b49ea2fa80619c08246866b22c9ad6eb6639
kare-tabetai <yahoo.zip.lzh.exe@gmail.com>
2024-11-10 10:30:08 +0900

    スワイプ動作カメラを途中まで作成

---
+++ Assets/ti_kare_/Script/CameraCOntroller.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraCOntroller : MonoBehaviour
+{
+    Vector2? prev_pos;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButton(1))
+        {
+
+        }
+    }
+}
